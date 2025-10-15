f7b1305e14ae6f90b171e99cbc88e7c575d33379
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-10-15 20:09:42 -0300

    chore: change name file

---
+++ src/product/query/product_find.query.ts
@@ -0,0 +1,44 @@
+import { ProductFindDto } from '../dto/product-find.dto';
+
+export function findProductQuery(dataJsonDto: ProductFindDto): string {
+  const olAppId = dataJsonDto.pe_app_id ?? 1;
+  const olSystemClientId = dataJsonDto.pe_system_client_id ?? 1;
+  const olStoreId = dataJsonDto.pe_store_id ?? 1;
+  const olOrganizationId = dataJsonDto.pe_organization_id ?? '';
+  const olMemberId = dataJsonDto.pe_member_id ?? '';
+  const olUserId = dataJsonDto.pe_user_id ?? '';
+  const olPersonId = dataJsonDto.pe_person_id ?? 0;
+
+  const olIdTaxonomy = dataJsonDto.pe_id_taxonomy ?? 0;
+  const olIdProduct = dataJsonDto.pe_id_produto ?? 0;
+  const olProduct = dataJsonDto.pe_produto ?? '';
+
+  const olFlagEstoque = dataJsonDto.pe_flag_estoque ?? 0;
+  const olFlagInativo = dataJsonDto.pe_flag_inativo ?? 0;
+
+  const olQtRegistros = dataJsonDto.pe_qt_registros ?? 100;
+  const olPaginaId = dataJsonDto.pe_pagina_id ?? 1;
+  const olColunaId = dataJsonDto.pe_coluna_id ?? 1;
+  const olOrderId = dataJsonDto.pe_ordem_id ?? 1;
+
+  const queryString = ` call sp_product_find_v2(
+    ${olAppId},
+    ${olSystemClientId},
+    ${olStoreId},
+    '${olOrganizationId}',
+    '${olMemberId}',
+    '${olUserId}',
+    ${olPersonId},
+    ${olIdTaxonomy},
+    ${olIdProduct},
+    '${olProduct}',
+    ${olFlagEstoque},
+    ${olFlagInativo},
+    ${olQtRegistros},
+    ${olPaginaId},
+    ${olColunaId},
+    ${olOrderId}
+      ) `;
+
+  return queryString;
+}
