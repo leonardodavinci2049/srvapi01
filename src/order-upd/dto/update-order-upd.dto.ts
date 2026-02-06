1d09c1699f717d19b84f9a90c3b28b68f45867e8
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-02-06 10:02:31 -0300

    chore: add dto files

--- src/order-upd/dto/update-order-upd.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateOrderUpdDto } from './create-order-upd.dto';
-
-export class UpdateOrderUpdDto extends PartialType(CreateOrderUpdDto) {}
