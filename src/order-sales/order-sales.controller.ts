4b873f402ff333e6f2c93ecc0531f7c5cbc1527e
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-02-09 11:28:07 -0300

    feat: add new order find and update endpoints with authentication and service integration

--- src/order-sales/order-sales.controller.ts
+++ src/order-sales/order-sales.controller.ts
@@ -1,5 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { OrderSalesService } from './order-sales.service';
+import { AuthGuard } from 'src/core/guards/auth.guard';
+import { OrderFindCoCarrierIdDto } from './dto/order-find-co-carrier-id.dto';
+import { OrderFindCoCustomerIdDto } from './dto/order-find-co-customer-id.dto';
+import { OrderFindCoDeliveryIdDto } from './dto/order-find-co-delivery-id.dto';
+import { OrderFindCoEquipmentIdDto } from './dto/order-find-co-equipment-id.dto';
+import { OrderFindCoHistoryIdDto } from './dto/order-find-co-history-id.dto';
+import { OrderFindCoNfIdDto } from './dto/order-find-co-nf-id.dto';
+import { OrderFindCoPgFormaIdDto } from './dto/order-find-co-pg-forma-id.dto';
+import { OrderFindCoProtocolIdDto } from './dto/order-find-co-protocol-id.dto';
+import { OrderFindCoSellerFindIdDto } from './dto/order-find-co-seller-find-id.dto';
+import { OrderFindCoSellerIdDto } from './dto/order-find-co-seller-id.dto';
+import { OrderFindCoSummaryIdDto } from './dto/order-find-co-summary-id.dto';
+import { OrdersFindDashboardIdDto } from './dto/orders-find-dashboard-id.dto';
 
 @Controller('order-sales')
 export class OrderSalesController {
@@ -15,8 +28,77 @@ export class OrderSalesController {
       timestamp: new Date().toISOString(),
       endpoints: {
         base: '/api',
-        auth: '/api/order-operation',
+        auth: '/api/order-sales',
       },
     };
+  }
+  @UseGuards(AuthGuard)
+  @Post('v2/order-find-dashboard-id')
+  OrderFindDashboardIdV2(@Body() dataJsonDto: OrdersFindDashboardIdDto) {
+    return this.orderSalesService.taskOrdersFindDashboardIdV2(dataJsonDto);
+  }
+  @UseGuards(AuthGuard)
+  @Post('v2/order-find-co-carrier-id')
+  OrderFindCoCarrierIdV2(@Body() dataJsonDto: OrderFindCoCarrierIdDto) {
+    return this.orderSalesService.taskOrderFindCoCarrierIdV2(dataJsonDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('v2/order-find-co-customer-id')
+  OrderFindCoCustomerIdV2(@Body() dataJsonDto: OrderFindCoCustomerIdDto) {
+    return this.orderSalesService.taskOrderFindCoCustomerIdV2(dataJsonDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('v2/order-find-co-delivery-id')
+  OrderFindCoDeliveryIdV2(@Body() dataJsonDto: OrderFindCoDeliveryIdDto) {
+    return this.orderSalesService.taskOrderFindCoDeliveryIdV2(dataJsonDto);
+  }
+  @UseGuards(AuthGuard)
+  @Post('v2/order-find-equipment-id')
+  OrderFindCoEquipmentIdV2(@Body() dataJsonDto: OrderFindCoEquipmentIdDto) {
+    return this.orderSalesService.taskOrderFindCoEquipmentIdV2(dataJsonDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('v2/order-find-co-history-id')
+  OrderFindCoHistoryIdV2(@Body() dataJsonDto: OrderFindCoHistoryIdDto) {
+    return this.orderSalesService.taskOrderFindCoHistoryIdV2(dataJsonDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('v2/order-find-co-nf-id')
+  OrderFindCoNfIdV2(@Body() dataJsonDto: OrderFindCoNfIdDto) {
+    return this.orderSalesService.taskOrderFindCoNfIdV2(dataJsonDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('v2/order-find-co-pg-forma-id')
+  OrderFindCoPgFormaIdV2(@Body() dataJsonDto: OrderFindCoPgFormaIdDto) {
+    return this.orderSalesService.taskOrderFindCoPgFormaIdV2(dataJsonDto);
+  }
+  @UseGuards(AuthGuard)
+  @Post('v2/order-find-co-protocol-id')
+  OrderFindCoProtocolIdV2(@Body() dataJsonDto: OrderFindCoProtocolIdDto) {
+    return this.orderSalesService.taskOrderFindCoProtocolIdV2(dataJsonDto);
+  }
+  @UseGuards(AuthGuard)
+  @Post('v2/order-find-co-seller-find-id')
+  OrderFindCoSellerOrderFindIdV2(
+    @Body() dataJsonDto: OrderFindCoSellerFindIdDto,
+  ) {
+    return this.orderSalesService.taskOrderFindCoSellerFindIdV2(dataJsonDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('v2/order-find-co-seller-id')
+  OrderFindCoSellerIdV2(@Body() dataJsonDto: OrderFindCoSellerIdDto) {
+    return this.orderSalesService.taskOrderFindCoSellerIdV2(dataJsonDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('v2/order-find-co-summary-id')
+  OrderFindCoSummaryIdV2(@Body() dataJsonDto: OrderFindCoSummaryIdDto) {
+    return this.orderSalesService.taskOrderFindCoSummaryIdV2(dataJsonDto);
   }
 }
--- src/order-upd/order-upd.service.ts
+++ src/order-upd/order-upd.service.ts
@@ -1,8 +1,204 @@
 import { Injectable } from '@nestjs/common';
+import { ResultModel } from 'src/core/utils/result.model';
+import { MESSAGES } from 'src/core/utils/constants/globalConstants';
 
+import { DatabaseService } from 'src/database/database.service';
+
+import { OrderUpdCustomerIdDto } from './dto/order-upd-customer-id.dto';
+import { OrderUpdDiscountDto } from './dto/order-upd-discount.dto';
+import { OrderUpdFreteDto } from './dto/order-upd-frete.dto';
+import { OrderUpdIncreaseVlDto } from './dto/order-upd-increase-vl.dto';
+import { OrderUpdNotesDto } from './dto/order-upd-notes.dto';
+import { OrderUpdPgMethodIdDto } from './dto/order-upd-pg-method-id.dto';
+import { OrderUpdSellerIdDto } from './dto/order-upd-seller-id.dto';
+import { OrderUpdStatusIdDto } from './dto/order-upd-status-id.dto';
+import { SpResultTaxonomyFindIdData } from './types/order-upd.type';
+
+import { OrderUpdCustomerIdQuery } from './query/order-upd-customer-id.query';
+import { OrderUpdFreteQuery } from './query/order-upd-frete.query';
+import { OrderUpdIncreaseVlQuery } from './query/order-upd-increase-vl.query';
+import { OrderUpdNotesQuery } from './query/order-upd-notes.query';
+import { OrderUpdPgMethodIdQuery } from './query/order-upd-pg-method-id.query';
+import { OrderUpdSellerIdQuery } from './query/order-upd-seller-id.query';
+import { OrderUpdStatusIdQuery } from './query/order-upd-status-id.query';
+import { OrderUpdDiscountQuery } from './query/order-upd-discount.query';
 @Injectable()
 export class OrderUpdService {
-  findAll() {
-    return `This action returns all orderSales`;
+  constructor(private readonly dbService: DatabaseService) {}
+
+  async taskOrderUpdCustomerIdV2(dataJsonDto: OrderUpdCustomerIdDto) {
+    try {
+      const queryString = OrderUpdCustomerIdQuery(dataJsonDto);
+
+      const resultData = (await this.dbService.selectExecute(
+        queryString,
+      )) as unknown as SpResultTaxonomyFindIdData;
+
+      return resultData;
+
+      /* 
+      return processProcedureResultMutation(
+        resultData as unknown[],
+        'Order Oper add item failed',
+      ); */
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : MESSAGES.UNKNOWN_ERROR;
+      return new ResultModel(100404, errorMessage, 0, []);
+    }
+  }
+
+  async taskOrderUpdDiscountV2(dataJsonDto: OrderUpdDiscountDto) {
+    try {
+      const queryString = OrderUpdDiscountQuery(dataJsonDto);
+
+      const resultData = (await this.dbService.selectExecute(
+        queryString,
+      )) as unknown as SpResultTaxonomyFindIdData;
+
+      return resultData;
+
+      /* 
+      return processProcedureResultMutation(
+        resultData as unknown[],
+        'Order Oper add item failed',
+      ); */
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : MESSAGES.UNKNOWN_ERROR;
+      return new ResultModel(100404, errorMessage, 0, []);
+    }
+  }
+
+  async taskOrderUpdFreteV2(dataJsonDto: OrderUpdFreteDto) {
+    try {
+      const queryString = OrderUpdFreteQuery(dataJsonDto);
+
+      const resultData = (await this.dbService.selectExecute(
+        queryString,
+      )) as unknown as SpResultTaxonomyFindIdData;
+
+      return resultData;
+
+      /* 
+      return processProcedureResultMutation(
+        resultData as unknown[],
+        'Order Oper add item failed',
+      ); */
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : MESSAGES.UNKNOWN_ERROR;
+      return new ResultModel(100404, errorMessage, 0, []);
+    }
+  }
+
+  async taskOrderUpdIncreaseVlV2(dataJsonDto: OrderUpdIncreaseVlDto) {
+    try {
+      const queryString = OrderUpdIncreaseVlQuery(dataJsonDto);
+
+      const resultData = (await this.dbService.selectExecute(
+        queryString,
+      )) as unknown as SpResultTaxonomyFindIdData;
+
+      return resultData;
+
+      /* 
+      return processProcedureResultMutation(
+        resultData as unknown[],
+        'Order Oper add item failed',
+      ); */
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : MESSAGES.UNKNOWN_ERROR;
+      return new ResultModel(100404, errorMessage, 0, []);
+    }
+  }
+
+  async taskOrderUpdNotesV2(dataJsonDto: OrderUpdNotesDto) {
+    try {
+      const queryString = OrderUpdNotesQuery(dataJsonDto);
+
+      const resultData = (await this.dbService.selectExecute(
+        queryString,
+      )) as unknown as SpResultTaxonomyFindIdData;
+
+      return resultData;
+
+      /* 
+      return processProcedureResultMutation(
+        resultData as unknown[],
+        'Order Oper add item failed',
+      ); */
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : MESSAGES.UNKNOWN_ERROR;
+      return new ResultModel(100404, errorMessage, 0, []);
+    }
+  }
+
+  async taskOrderUpdPgMethodIdV2(dataJsonDto: OrderUpdPgMethodIdDto) {
+    try {
+      const queryString = OrderUpdPgMethodIdQuery(dataJsonDto);
+
+      const resultData = (await this.dbService.selectExecute(
+        queryString,
+      )) as unknown as SpResultTaxonomyFindIdData;
+
+      return resultData;
+
+      /* 
+      return processProcedureResultMutation(
+        resultData as unknown[],
+        'Order Oper add item failed',
+      ); */
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : MESSAGES.UNKNOWN_ERROR;
+      return new ResultModel(100404, errorMessage, 0, []);
+    }
+  }
+
+  async taskOrderUpdSellerIdV2(dataJsonDto: OrderUpdSellerIdDto) {
+    try {
+      const queryString = OrderUpdSellerIdQuery(dataJsonDto);
+
+      const resultData = (await this.dbService.selectExecute(
+        queryString,
+      )) as unknown as SpResultTaxonomyFindIdData;
+
+      return resultData;
+
+      /* 
+      return processProcedureResultMutation(
+        resultData as unknown[],
+        'Order Oper add item failed',
+      ); */
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : MESSAGES.UNKNOWN_ERROR;
+      return new ResultModel(100404, errorMessage, 0, []);
+    }
+  }
+
+  async taskOrderUpdStatusIdV2(dataJsonDto: OrderUpdStatusIdDto) {
+    try {
+      const queryString = OrderUpdStatusIdQuery(dataJsonDto);
+
+      const resultData = (await this.dbService.selectExecute(
+        queryString,
+      )) as unknown as SpResultTaxonomyFindIdData;
+
+      return resultData;
+
+      /* 
+      return processProcedureResultMutation(
+        resultData as unknown[],
+        'Order Oper add item failed',
+      ); */
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : MESSAGES.UNKNOWN_ERROR;
+      return new ResultModel(100404, errorMessage, 0, []);
+    }
   }
 }
