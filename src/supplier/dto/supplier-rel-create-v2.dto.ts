d0018756d0cecbe5e8628524d3bb9d316b152b06
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-02-15 11:21:15 -0300

    refactor: clean up code formatting and improve DTOs for supplier management
    - Standardized code formatting across app.controller.spec.ts, app.controller.ts, app.module.ts, app.service.ts, and envs.ts.
    - Enhanced Supplier DTOs (Create, Delete, Find, Update) with additional properties and sample JSON for testing.
    - Improved error handling and logging in DatabaseService.
    - Updated global constants for better clarity and organization.

--- src/supplier/dto/supplier-rel-create-v2.dto.ts
+++ src/supplier/dto/supplier-rel-create-v2.dto.ts
@@ -33,4 +33,35 @@ export class SupplierRelCreateV2Dto {
   @ApiProperty({ description: 'Person ID' })
   @IsNumber()
   pe_person_id: number;
+
+  @ApiProperty({ description: 'Supplier ID' })
+  @IsNumber()
+  pe_id_supplier: number;
+
+  @ApiProperty({ description: 'Product ID' })
+  @IsNumber()
+  pe_id_product: number;
+
+  @ApiProperty({ description: 'Supplier Code', maxLength: 200 })
+  @IsString()
+  pe_supplier_code: string;
 }
+
+/*
+Sample JSON for testing in body endpoint:
+
+{
+  "pe_app_id": 1,
+  "pe_system_client_id": 1,
+  "pe_store_id": 3,
+  "pe_organization_id": "ORG001",
+  "pe_member_role": "saller",
+  "pe_user_id": "USER001",
+  "pe_person_id": 29014,
+  "pe_id_supplier": 123,
+  "pe_id_product": 456,
+  "pe_supplier_code": "SUPP-001"
+
+}
+
+*/
--- src/supplier/dto/supplier-update-v2.dto.ts
+++ src/supplier/dto/supplier-update-v2.dto.ts
@@ -33,4 +33,40 @@ export class SupplierUpdateV2Dto {
   @ApiProperty({ description: 'Person ID' })
   @IsNumber()
   pe_person_id: number;
+
+  @ApiProperty({ description: 'Supplier ID' })
+  @IsNumber()
+  pe_id_supplier: number;
+
+  @ApiProperty({ description: 'Supplier', maxLength: 100 })
+  @IsString()
+  pe_supplier: string;
+
+  @ApiProperty({ description: 'Notes' })
+  @IsString()
+  pe_notes: string;
+
+  @ApiProperty({ description: 'Inactive' })
+  @IsNumber()
+  pe_inactive: number;
 }
+
+/*
+Sample JSON for testing in body endpoint:
+
+{
+  "pe_app_id": 1,
+  "pe_system_client_id": 1,
+  "pe_store_id": 3,
+  "pe_organization_id": "ORG001",
+  "pe_member_role": "saller",
+  "pe_user_id": "USER001",
+  "pe_person_id": 29014,
+  "pe_id_supplier": 123,
+  "pe_supplier": "Supplier Name",
+  "pe_notes": "Some notes about the supplier",
+  "pe_inactive": 0
+
+}
+
+*/
