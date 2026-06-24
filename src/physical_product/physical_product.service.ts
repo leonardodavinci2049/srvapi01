ad3514ea5de0fe4d24b6bf16c16cbc1052d77dac
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-06-24 15:53:39 -0300

    refactor: remove unused CRUD operations from physical product service and controller

--- src/physical_product/physical_product.service.ts
+++ src/physical_product/physical_product.service.ts
@@ -1,26 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePhysicalProductDto } from './dto/create-physical_product.dto';
-import { UpdatePhysicalProductDto } from './dto/update-physical_product.dto';
 
 @Injectable()
 export class PhysicalProductService {
-  create(createPhysicalProductDto: CreatePhysicalProductDto) {
-    return 'This action adds a new physicalProduct';
-  }
-
   findAll() {
     return `This action returns all physicalProduct`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} physicalProduct`;
-  }
-
-  update(id: number, updatePhysicalProductDto: UpdatePhysicalProductDto) {
-    return `This action updates a #${id} physicalProduct`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} physicalProduct`;
   }
 }
