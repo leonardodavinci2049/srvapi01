831fc4718160c5e1ff6885bb1677646d077edc77
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-08-05 13:38:08 -0300

    Refactor DTOs and queries: update properties and add optional fields; enhance logging for query generation

--- src/order-b2b/types/order-b2b.type.ts
+++ src/order-b2b/types/order-b2b.type.ts
@@ -93,6 +93,8 @@ export interface TblCustomerDetails extends RowDataPacket {
   PATH_IMAGEM?: string;
   DATADOCADASTRO?: Date;
   DT_ULTIMA_COMPRA?: Date;
+  FLAG_FRETE_GRATIS?: number;
+  VL_PP_DESCONTO?: number; 
   FONE1?: string;
   WHATAPP1?: string;
   EMAIL?: string;
--- src/order-sales/query/order-find-co-tipo-frete.query.ts
+++ src/order-sales/query/order-find-co-tipo-frete.query.ts
@@ -10,13 +10,15 @@ export function OrderFindCoTipoFreteQuery(
       tbl_pedido_tipo_frete.VALOR_PADRAO
     FROM
       tbl_pedido_tipo_frete
-    WHERE
-      tbl_pedido_tipo_frete.ID_SYSTEM_CLIENTE = ${dataJsonDto.pe_system_client_id}
-      AND tbl_pedido_tipo_frete.ID_LOJA = ${dataJsonDto.pe_store_id}
+    WHERE 1 = 1
       AND tbl_pedido_tipo_frete.INATIVO = 0
     ORDER BY tbl_pedido_tipo_frete.TIPO_FRETE ASC
     LIMIT 10
   `;
 
+  // console.log('Generated Query 1:', queryString);
+
   return queryString;
 }
+// AND  tbl_pedido_tipo_frete.ID_SYSTEM_CLIENTE = ${dataJsonDto.pe_system_client_id}
+ //     AND tbl_pedido_tipo_frete.ID_LOJA = ${dataJsonDto.pe_store_id}
