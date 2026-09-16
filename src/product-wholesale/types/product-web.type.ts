0ecf169b5830b0da38464a6a26da1c4cbe3ff677
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-09-16 15:06:58 -0300

    feat(product-wholesale): add taxonomy find menu v2 endpoint and related DTO, service, and query

--- src/product-wholesale/types/product-web.type.ts
+++ src/product-wholesale/types/product-web.type.ts
@@ -144,6 +144,22 @@ export interface tbltaxonomyWebRelated extends RowDataPacket {
   LEVEL?: number;
 }
 
+export interface TblProductWholesaleTaxonomyFindMenu extends RowDataPacket {
+  ID_TAXONOMY: number;
+  PARENT_ID: number;
+  TAXONOMIA: string;
+  PATH_IMAGEM: string | null;
+  SLUG: string | null;
+  LEVEL: number | null;
+  ORDEM: number;
+  INATIVO: number;
+  QTY_PRODUCTS: number;
+}
+
+export interface TblProductWholesaleTaxonomyQuantity extends RowDataPacket {
+  QTY_TAXONOMIES: number;
+}
+
 export type SpResultRecordCreateType = [SpDefaultFeedback[], SpOperationResult];
 export type SpResultRecordUpdateType = [SpDefaultFeedback[], SpOperationResult];
 export type SpResultRecordDeleteType = [SpDefaultFeedback[], SpOperationResult];
@@ -164,6 +180,13 @@ export type SpProductWebFindDataType = [
 
 export type SpProductWebSectionsDataType = [
   tblProductWebSections[],
+  SpDefaultFeedback[],
+  SpOperationResult,
+];
+
+export type SpProductWholesaleTaxonomyFindMenuDataType = [
+  TblProductWholesaleTaxonomyFindMenu[],
+  TblProductWholesaleTaxonomyQuantity[],
   SpDefaultFeedback[],
   SpOperationResult,
 ];
